551938baf2a2d3dff7bc4dfdc23fe346c305723f
Juan Rivera <juanriv45@gmail.com>
2019-03-07 02:43:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
   get  '/square/:number' do
     @number = params[:number].to_i
-    @number = @number * @number
-    "#{@number}"
+    @number_squared = @number * @number
+    "#{@number_squared}"
   end
 end
