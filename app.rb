92e70b100e50246ea292e6482e9849ad714691ea
Juan Rivera <juanriv45@gmail.com>
2019-03-07 02:38:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-get '/reversename/:name' do
-  @name = param[:name].to_s.reverse
-   puts "#{@name}"
-end
+  get '/reversename/:name' do
+    @name = param[:name].to_s.reverse
+     puts "#{@name}"
+  end
 end
